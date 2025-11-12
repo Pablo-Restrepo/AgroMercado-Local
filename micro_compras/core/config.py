5028825600bd964bd6ef23b3fdb21030d56342d4
Guejia21 <jonathanguejia@unicauca.edu.co>
2025-11-11 21:16:42 -0500

    pequeños cambios en el compose para que corriera el compose

--- micro_compras/core/config.py
+++ micro_compras/core/config.py
@@ -2,16 +2,12 @@ from pydantic_settings import BaseSettings
 from pydantic import ConfigDict
 
 
-class Settings(BaseSettings):    
-    BASE_URL: str = 'http://localhost:8000'
-    PROJECT_NAME: str = 'AgroMercado-Local'
-    DATABASE_URL: str     
-    RABBIT_URL: str 
-    QUEUE_NAME: str 
-    PRODUCTORS_QUEUE: str
+class Settings(BaseSettings):
+    RABBIT_URL: str = 'amqp://guest:guest@localhost:5672/'
+    QUEUE_NAME: str     
     SECRET_KEY: str
-    ALGORITHM: str     
-    EUREKA_APP_NAME: str
+    ALGORITHM: str 
+    EUREKA_APP_NAME: str 
     EUREKA_INSTANCE_PORT: int 
     EUREKA_INSTANCE_HOST: str 
     EUREKA_SERVER_URL: str 
